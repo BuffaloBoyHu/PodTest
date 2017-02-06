64609fff6ca6a45322167c2714efeefb0049fbf6
buffalohlh <buffaloboyhu@163.com>
2017-02-06 22:33:10 +0800

    Pod test

---
+++ PodTest/TestDemo.swift
@@ -0,0 +1,16 @@
+//
+//  TestDemo.swift
+//  PodTestDemo
+//
+//  Created by hu lianghai on 2017/2/6.
+//  Copyright © 2017年 hu lianghai. All rights reserved.
+//
+
+import UIKit
+
+class TestDemo: NSObject {
+
+    public func printDemo() {
+        print("this is pod test")
+    }
+}
